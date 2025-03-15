a7dddb3daf447d5f547d15be744b8e75301e62f3
Parvej Khan <iamparvejkhan03@gmail.com>
2025-03-15 16:36:59 +0530

    Made a few changes

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,6 +1,6 @@
 function App(){
     return (
-        <h1 className="text-green-800 text-3xl font-extrabold text-center italic selection:text-green-500 selection:bg-green-100">Hello, My name is Parvej Khan!</h1>
+        <h1 className="text-red-800 text-3xl font-extrabold text-center italic selection:text-green-500 selection:bg-green-100">Hello, My name is Parvej Khan!</h1>
     );
 }
 
