339a113848f79b219f8c6455e10338c4dc038ba9
Parvej Khan <iamparvejkhan03@gmail.com>
2025-03-15 16:05:31 +0530

    Added all the files

---
+++ vite/src/App.jsx
@@ -0,0 +1,7 @@
+function App(){
+    return (
+        <h1 className="text-green-800 text-3xl font-extrabold text-center italic selection:text-green-500 selection:bg-green-100">Hello, My name is Parvej Khan!</h1>
+    );
+}
+
+export default App;
