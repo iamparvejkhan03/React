8f69552de0ded060f05aab948a5b7937efefbbf0
Parvej Khan <iamparvejkhan03@gmail.com>
2025-05-20 09:39:13 +0530

    for vercel

--- hotelBooking/server/controllers/clerkWebhooks.js
+++ hotelBooking/server/controllers/clerkWebhooks.js
@@ -1,6 +1,45 @@
-const clerkWebhooks = (req, res) => {
+import { Webhook } from 'svix';
+import User from '../models/User';
+
+const clerkWebhooks = async (req, res) => {
     try{
-        
+        const wHook = new Webhook(process.env.CLERK_WEBHOOK_SIGNING_SECRET);
+
+        const headers = {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
+        }
+
+        await wHook.verify(JSON.stringify(req.body), headers);
+
+        const {data, type} = req.body;
+
+        const userData = {
+            _id: data.id,
+            username: `${data.first_name} ${data.last_name}`,
+            email: data.email_addresses[0].email_address,
+            image: data.image_url,
+        }
+
+        switch(type){
+            case "user.created":
+                await User.create(userData);
+                break;
+
+            case "user.updated":
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+
+            case "user.deleted":
+                await User.findByIdAndDelete(data.id);
+                break;
+
+            default:
+                break;
+        }
+
+        res.json({success: true, message: "Webhook reached."});
     }catch(error){
         res.json({success:false, message:error.message});
     }
